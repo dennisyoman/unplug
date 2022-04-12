c8b25638e1db8cd0427d1562fbf76dc97d5ca870
Dennisyoman Hsueh <dennisyoman@gmail.com>
2022-04-12 16:31:27 +0800

    sky fix

--- web/DATA/JS/organize.js
+++ web/DATA/JS/organize.js
@@ -40,19 +40,21 @@ $(document).ready(function () {
       }
 
       //sidetool
-      $(".sideTool > div.btn_answer")
+
+      $(".sideTool > div.btn_replay")
         .unbind()
         .bind("click", function () {
-          $(this).toggleClass("active");
-          if ($(this).hasClass("active")) {
-            /*rootSoundEffectName(
-              $(".contents > div.selected").find("audio").attr("src"),
-              true
-            );*/
-          } else {
-            /*closePlayer();*/
-          }
+          $(this).hide();
+          resetElem($(".contents > div.selected"));
         });
+
+      //build grid
+      $(".grids > .row").each(function () {
+        var size = $(this).attr("size");
+        for (var i = 0; i < size; i++) {
+          $(this).append(`<span style="width:${bw}px;height:${bh}px;"/>`);
+        }
+      });
 
       //init
 
@@ -68,7 +70,6 @@ $(document).ready(function () {
 
   //check loading
   checkCompLoading("#module_wrapper");
-
   $("#module_wrapper .units-title")
     .addClass("l" + lid)
     .text(getLessonName());
@@ -80,6 +81,9 @@ var lastPosX = 0;
 var lastPosY = 0;
 var isDragging = false;
 var $elem = null;
+//
+var bw = 35;
+var bh = 28;
 
 var trigHammer = function () {
   //hammer
@@ -103,25 +107,138 @@ var trigHammer = function () {
 };
 
 var handleDrag = function (ev) {
-  var elem = ev.target;
-
   if (!isDragging && $elem != null) {
     isDragging = true;
-    lastPosX = $elem.offsetLeft;
-    lastPosY = $elem.offsetTop;
+    if ($($elem).hasClass("draggable")) {
+      $("#module_wrapper").append(
+        `<div id="cardAvatar" class="cardAvatar"></div>`,
+      );
+      $($elem).find(">img").clone().appendTo("#cardAvatar");
+      $("#cardAvatar").attr("size", $($elem).attr("size"));
+      $($elem).addClass("cached");
+    }
   }
 
-  var posX = ev.deltaX / stageRatioReal + lastPosX;
-  var posY = ev.deltaY / stageRatioReal + lastPosY;
-
   if (isDragging && $elem) {
-    $elem.style.left = posX + "px";
-    $elem.style.top = posY + "px";
+    //drag clon card
+    if ($($elem).hasClass("draggable")) {
+      var deltaContainerX = $("#module_wrapper").offset().left;
+      var deltaContainerY = $("#module_wrapper").offset().top;
+      $("#cardAvatar").get(0).style.top =
+        Math.round(
+          ev.center.y / stageRatioReal -
+            deltaContainerY / stageRatioReal -
+            $("#cardAvatar").height() / stageRatioReal / 2,
+        ) + "px";
+      $("#cardAvatar").get(0).style.left =
+        Math.round(
+          ev.center.x / stageRatioReal -
+            deltaContainerX / stageRatioReal -
+            $("#cardAvatar").width() / stageRatioReal / 2,
+        ) + "px";
+      checkCollision(ev);
+    }
   }
 
   if (ev.isFinal) {
+    if ($($elem).hasClass("draggable")) {
+      var frameElem = $(".contents > div.selected .gem_container > div");
+      var gotit = false;
+      frameElem.each(function () {
+        if ($(this).hasClass("selected")) {
+          gotit = true;
+        }
+      });
+      //checkCollision true
+      if (gotit) {
+        //check order status
+        checkOrderStatus();
+      } else {
+        $("#cardAvatar").remove();
+        $(".cached").removeClass("cached");
+      }
+    }
+    //then
     isDragging = false;
     $elem = null;
+  }
+};
+
+var checkCollision = function (ev) {
+  var lastW = parseInt($($elem).attr("size")) * bw * stageRatioReal;
+  var lastX = ev.center.x - lastW / 2;
+  var lastY = ev.center.y;
+  var buffer = 3;
+
+  var frameElem = $(".contents > div.selected .grids > .row > span");
+  var gotit = false;
+  frameElem.each(function () {
+    var oriX = $(this).offset().left;
+    var oriW = $(this).width();
+    var oriY = $(this).offset().top;
+    var oriH = oriY + $(this).height();
+    if (
+      lastX < oriX + oriW / 2 - buffer &&
+      lastX > oriX - lastW + oriW / 2 + buffer &&
+      lastY >= oriY + buffer &&
+      lastY <= oriH - buffer
+    ) {
+      $(this).addClass("selected");
+      gotit = true;
+    } else {
+      $(this).removeClass("selected");
+    }
+  });
+  if (gotit) {
+    $("#cardAvatar").addClass("focus");
+  } else {
+    $("#cardAvatar").removeClass("focus");
+  }
+};
+
+var checkOrderStatus = function () {
+  var gridElem = $(".contents > div.selected .gem_container");
+  var tempNum = gridElem.find(">div.selected").attr("ans");
+  var tempNumCard = $("#cardAvatar").attr("ans");
+  if (tempNum == tempNumCard) {
+    //right
+    $(".sideTool > div.btn_replay").show();
+    rootSoundEffect($chimes);
+
+    var uniq = new Date().getTime();
+    gridElem
+      .find(">div.selected")
+      .removeClass("selected")
+      .addClass("bingo")
+      .append(
+        `<span class="smoke"><img src="./DATA/IMAGES/common/chimes.gif?uniq=${uniq}"/></span>`,
+      );
+    $("#cardAvatar").remove();
+    $(".cached")
+      .removeClass("cached")
+      .delay(800)
+      .queue(function () {
+        $(".smoke").remove();
+        $(this).dequeue();
+      });
+  } else {
+    //wrong
+    rootSoundEffect($fail);
+    var uniq = new Date().getTime();
+    gridElem
+      .find(">div.selected")
+      .removeClass("selected")
+      .append(
+        `<span class="smoke"><img src="./DATA/IMAGES/common/smoke.gif?uniq=${uniq}"/></span>`,
+      );
+    $("#cardAvatar").remove();
+    $(".cached")
+      .removeClass("cached")
+      .delay(800)
+      .queue(function () {
+        $(".smoke").remove();
+        $(this).dequeue();
+      });
   }
 };
 
@@ -134,21 +251,15 @@ var openContent = function (id) {
     .siblings(".selected")
     .removeClass("selected");
   resetElem($(".contents > div.selected"));
-  //show side tool btn
-  $(".sideTool > div.btn_answer").show();
 };
 
 var resetElem = function (elem) {
-  var ft = ($("#root").height() - elem.height()) / 2 / stageRatioReal - 25;
-  if (ft < 50) {
-    ft = 50;
-  }
-  var styles = {
-    transform: "none",
-    top: ft,
-    left: 260,
-  };
-  //elem.css(styles);
+  elem.find(".selected").removeClass("selected");
+  elem.find(".cached").removeClass("cached");
+
+  //smoke effect
+  $(".smoke").remove();
+  $(".cardAvatar").remove();
 };
 
 var resetTool = function () {
